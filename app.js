1426185a7419cdccc61242729040b0110618b445
Theekshana Wijesinghe <theekshanawjdev@gmail.com>
2020-02-15 12:11:15 +0530

    Add React dependencies to the app and create a sample React app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 
 const Main = () => {
-	return "<h2> Welcome React </h2>";
+	return <h2> Welcome React </h2>;
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
